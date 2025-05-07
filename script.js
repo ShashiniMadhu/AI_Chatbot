3037c9fd2c6550b3049c7e0a03683fc81e3ab06a
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-05-08 00:59:10 +0530

    let API KEY adding

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 //set up the API
-const API_KEY = "AIzaSyBmHVQdcDxQdO_U-uFCHqksHluxaCwAmMw";
+const API_KEY = "YOUR_GEMINI_API_KEY";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const userData={
