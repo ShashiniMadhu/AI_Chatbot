7c320a2c15aa5d695dcb6477dcd8c58ac2a898b8
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-05-07 17:00:33 +0530

    set up the emoji picker

--- script.js
+++ script.js
@@ -135,6 +135,27 @@ fileCancedlButton.addEventListener("click", () =>{
     fileUploadWrapper.classList.remove("file-uploaded");
 })
 
+//Initialize emoji picker and handle emoji selection
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition:"none",
+    previewPosition: "none",
+    onEmojiSelect:(emoji) => {
+        const{selectionstart:start, selectionend: end} = messageInput;
+        messageInput.setRangeText(emoji.native,start,end,"end");
+        messageInput.focus();
+    },
+    onClickOutside: (e) =>{
+        if(e.target.id === "emoji-picker"){
+           document.body.classList.toggle("show-emoji-picker"); 
+        }else{
+            document.body.classList.remove("show-emoji-picker"); 
+        }
+    }
+});
+
+document.querySelector(".chat-form").appendChild(picker);
+
 sendMessageButton.addEventListener("click",(e) => handleOutgoingMessage(e));
 document.querySelector("#file-upload").addEventListener("click",()=> fileInput.click());
 
